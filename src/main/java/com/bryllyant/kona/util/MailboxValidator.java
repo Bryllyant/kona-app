debaabe5f30405775aba123fcaa92e565a7ba2c4
Sharif Alexandre <sharif@fabriq.io>
2018-06-02 15:01:17 -0400

    updates

--- src/main/java/com/bryllyant/kona/util/MailboxValidator.java
+++ src/main/java/com/bryllyant/kona/util/MailboxValidator.java
@@ -102,58 +102,65 @@ public class MailboxValidator {
 	private static List<String> getMX(String hostName) throws NamingException {
 	    Optional<List<String>> result = mxCache.getIfPresent(hostName);
 
-	    // we don't have a value yet for this key
-	    if (result == null) {
-            // Perform a DNS lookup for MX records in the domain
-            Hashtable<String, String> env = new Hashtable<>();
+	    try {
 
-            env.put("java.naming.factory.initial",
-                    "com.sun.jndi.dns.DnsContextFactory");
+            // we don't have a value yet for this key
+            if (result == null) {
+                // Perform a DNS lookup for MX records in the domain
+                Hashtable<String, String> env = new Hashtable<>();
 
-            DirContext ictx = new InitialDirContext(env);
-            Attributes attrs = ictx.getAttributes(hostName, new String[]{"MX"});
-            Attribute attr = attrs.get("MX");
+                env.put("java.naming.factory.initial",
+                        "com.sun.jndi.dns.DnsContextFactory");
 
-            // if we don't have an MX record, try the machine itself
-            if ((attr == null) || (attr.size() == 0)) {
-                attrs = ictx.getAttributes(hostName, new String[]{"A"});
+                DirContext ictx = new InitialDirContext(env);
+                Attributes attrs = ictx.getAttributes(hostName, new String[]{"MX"});
+                Attribute attr = attrs.get("MX");
 
-                attr = attrs.get("A");
+                // if we don't have an MX record, try the machine itself
+                if ((attr == null) || (attr.size() == 0)) {
+                    attrs = ictx.getAttributes(hostName, new String[]{"A"});
 
-                if (attr == null) {
-                    // add empty value to know we've processed this domain
-                    mxCache.put(hostName, Optional.empty());
-                    throw new NamingException("No match for name '" + hostName + "'");
+                    attr = attrs.get("A");
+
+                    if (attr == null) {
+                        // add empty value to know we've processed this domain
+                        mxCache.put(hostName, Optional.empty());
+                        throw new NamingException("No match for name '" + hostName + "'");
+                    }
                 }
+
+                // Huzzah! we have machines to try. Return them as an array list
+                // NOTE: We SHOULD take the preference into account to be absolutely
+                // correct. This is left as an exercise for anyone who cares.
+                List<String> records = new ArrayList<>();
+
+                NamingEnumeration<?> en = attr.getAll();
+
+                while (en.hasMore()) {
+                    String mailhost;
+                    String x = (String) en.next();
+                    String f[] = x.split(" ");
+                    // THE fix *************
+                    if (f.length == 1)
+                        mailhost = f[0];
+                    else if (f[1].endsWith("."))
+                        mailhost = f[1].substring(0, (f[1].length() - 1));
+                    else
+                        mailhost = f[1];
+                    // THE fix *************
+                    records.add(mailhost);
+                }
+
+                logger.debug("MX hosts:\n" + KStringUtil.join(records, "\n"));
+
+                // cannot set null value using Optional.of().  user Optional.ofNullable() if the value can be null.
+                result = Optional.of(records);
+
+                mxCache.put(hostName, result);
             }
-
-            // Huzzah! we have machines to try. Return them as an array list
-            // NOTE: We SHOULD take the preference into account to be absolutely
-            // correct. This is left as an exercise for anyone who cares.
-            List<String> records = new ArrayList<>();
-
-            NamingEnumeration<?> en = attr.getAll();
-
-            while (en.hasMore()) {
-                String mailhost;
-                String x = (String) en.next();
-                String f[] = x.split(" ");
-                // THE fix *************
-                if (f.length == 1)
-                    mailhost = f[0];
-                else if (f[1].endsWith("."))
-                    mailhost = f[1].substring(0, (f[1].length() - 1));
-                else
-                    mailhost = f[1];
-                // THE fix *************
-                records.add(mailhost);
-            }
-
-            logger.debug("MX hosts:\n" + KStringUtil.join(records, "\n"));
-
-            // cannot set null value using Optional.of().  user Optional.ofNullable() if the value can be null.
-            result = Optional.of(records);
-
+        } catch (Throwable t) {
+	        logger.debug(t.getMessage());
+	        result = Optional.empty();
             mxCache.put(hostName, result);
         }
 
